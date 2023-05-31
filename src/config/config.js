cc4c5eaab067cf565311c495b5442f8e254a6774
Baobaotv <nguyenbaowwe@gmail.com>
2023-06-01 00:41:25 +0700

    hot fix domain

--- src/config/config.js
+++ src/config/config.js
@@ -138,11 +138,11 @@ const adminUrl = '/admin/home';
 
 const doctorUrl = '/admin/home';
 
-const hostBe = `http://${process.env.REACT_APP_HOST}`;
+const hostBe = `${process.env.REACT_APP_HOST}`;
 
 // const baseUrl = 'https://192.168.1.239:8443/api/';
 // const baseUrl = 'http://172.16.17.194:8080/api/';
-const baseUrl = `http://${process.env.REACT_APP_HOST}/api/`;
+const baseUrl = `${process.env.REACT_APP_HOST}/api/`;
 
 const constant = {
     payment_paid: 1,
